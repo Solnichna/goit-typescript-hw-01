9d22d16edd8adba5c985c09ebbce96ba23d1b321
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-05-07 19:50:29 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,10 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+function add(num1: number, num2: number) {
+     return num1 + num2;
+    }
+    
+    console.log(add(1, 2));
+    
